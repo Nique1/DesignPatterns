dcc5ca7d1a655f544923180d6e83bd4c1cb2eafb
Nique1 <dominikasarata@gmail.com>
2023-10-18 12:54:30 +0200

    Implementation of builder design pattern using inner class

--- src/package1/Main.java
+++ src/package1/Main.java
@@ -1,17 +1,23 @@
 package package1;
 
+import package1.flightMonitoring.FlightLeg;
 import package1.house.House;
 
 public class Main {
 
     public static void main(String[] args) {
-        House house = new House.HouseBuilder()
-                .buildWalls("walls")
-                .buildRooms("room")
-                .buildWindows("windows")
+        FlightLeg flightLeg = new FlightLeg.FlightLegBuilder()
+                .buildFrom("Los Angeles")
+                .buildTo("Las Vegas")
+                .buildPrice(50.0)
                 .build();
+//        FlightLeg flightLeg1 = new FlightLeg.FlightLegBuilder()
+//                .buildFrom("New York")
+//                .buildTo("Warsaw")
+//                .build();
 
-        System.out.println(house);
-
+        System.out.println(flightLeg);
+//        System.out.println(flightLeg1);
     }
+
 }
